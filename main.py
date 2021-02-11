948fcf0328f5b44421bae4336129f5dc098fb265
David Cruz Anaya <me@davidcruz.co.uk>
2021-02-11 23:55:42 +0000

    extact all information and print in to the console

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 
 for x in range(1, 51):
     url = "http://books.toscrape.com/catalogue/category/books_1/page-" + str(x) + ".html"
@@ -14,15 +14,15 @@ for x in range(1, 51):
     # get a href: row.find(class_="image_container").find('a')['href'] == book url
     # get img src: row.find(class_="image_container").find('img')['src'] == book image
 
-    product_page_url = ""
-    universal_product_code = ""
-    title = ""
-    price_including_tax = ""
-    price_excluding_tax = ""
-    number_available = ""
-    product_description = ""
-    category = ""
-    review_rating = ""
+    product_page_url = ""  # done
+    universal_product_code = ""  # done
+    title = ""  # done
+    price_including_tax = ""  # done
+    price_excluding_tax = ""  # done
+    number_available = ""  # done
+    product_description = ""  # done
+    category = ""  # done
+    review_rating = ""  # done
     image_url = ""
 
     list_of_books = soup.find_all(class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
@@ -32,7 +32,9 @@ for x in range(1, 51):
         # print("product_page_url: " + product_page_url)
 
         url_book = book.find(class_="image_container").find('a')['href'].replace("../../", '')
-        product_page_url = "http://books.toscrape.com/catalogue/" + url_book
+        base_url = "http://books.toscrape.com/"
+        product_page_url = base_url + "catalogue/" + url_book
+        print("product_page_url: " + product_page_url)
         product_page_request = requests.get(product_page_url)
         current_book_soup = BeautifulSoup(product_page_request.content, 'html.parser')
 
@@ -40,9 +42,52 @@ for x in range(1, 51):
         if page.status_code != 200:
             continue
 
-        title = current_book_soup.find(class_="col-sm-6 product_main").find('h1').get_text()
+        product_description = current_book_soup.find('p', class_=False).get_text()
+        print("product_description:" + product_description)
 
+        tables = current_book_soup.find('table', class_="table table-striped")
+
+        for table in tables:
+            # skip NavigableString rows
+            if isinstance(table, NavigableString):
+                continue
+
+            th = str(table.find('th').get_text())
+            td = str(table.find('td').get_text())
+            if th == "UPC":
+                universal_product_code = td
+                print("universal_product_code: " + universal_product_code)
+
+            if th == "Price (incl. tax)":
+                price_including_tax = td
+                print("price_including_tax: " + price_including_tax)
+
+            if th == "Price (excl. tax)":
+                price_excluding_tax = td
+                print("price_excluding_tax: " + price_excluding_tax)
+
+            if th == "Availability":
+                def extract_stock(numbers: str) -> str:
+                    stock = numbers.split()
+                    for number in stock:
+                        num = number.replace("(", "")
+                        if num.isdigit():
+                            return str(num)
+
+
+                number_available = extract_stock(td)
+                print("number_available: " + number_available)
+
+            if th == "Product Type":
+                category = td
+                print("category: " + category)
+
+            if th == "Number of reviews":
+                review_rating = td
+                print("review_rating: " + review_rating)
+
+        title = current_book_soup.find(class_="col-sm-6 product_main").find('h1').get_text()
         print("title: " + title)
 
-        # image_url = book.find(class_="image_container").find('img')['src']
-        # print("image_url: " + image_url)
+        image_url = base_url + current_book_soup.find('img')['src'].replace("../../", '')
+        print("image_url: " + image_url)
